2e597c82c235ed1f29ad2d09f3a8890ca6523849
turchimatteo <turchi12@gmail.com>
2022-02-10 15:22:47 +0100

    header completato

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,26 @@
+<header>
+    <div class="first-header"></div>
+    <div class="second-header">
+        <div class="container">
+            <div class="wrapper">
+                <div class="logo">
+                    <img src="{{ asset('img/dc-logo.png') }}" alt="Logo DC">
+                </div>
+                <nav>
+                    <ul class="menu">
+                        <li>CHARACTERS</li>
+                        <li class="current">COMICS</li>
+                        <li>MOVIES</li>
+                        <li>TV</li>
+                        <li>GAMES</li>
+                        <li>COLLECTIBLES</li>
+                        <li>VIDEOS</li>
+                        <li>FANS</li>
+                        <li>NEWS</li>
+                        <li>SHOP</li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
